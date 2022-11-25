e14a15b8d60b358ff3c8a9d71324361f2de7e016
YoshuaG <yoshua.gar@gmail.com>
2022-11-25 19:15:21 -0300

    exercicio1

---
+++ Fundamento/Java/esxercicio1.js
@@ -0,0 +1,10 @@
+const  a = 20
+const b = 10
+
+console.log('soma' + (b + a));
+console.log('multiplicação' + (b * a));
+console.log('subtração' + (b - a));
+console.log('divisão' + (b / a));
+console.log('Módulo' + (b % a));
+
+// =)
