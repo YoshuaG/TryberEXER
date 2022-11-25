f8560e59734593eed915f4cc6f452eaaa0c98dea
YoshuaG <yoshua.gar@gmail.com>
2022-11-25 19:42:29 -0300

    exercicio4

---
+++ Fundamento/Java/exercicio4.js
@@ -0,0 +1,19 @@
+// Utilize if/else para escrever um código que, 
+// dado um valor recebido como parâmetro
+// retorne “positive” se esse valor for positivo,
+// “negative” se for negativo, e caso não seja nem positivo
+//  e nem negativo retorne “zero”.
+
+const number = 11;
+
+if (number > 0) {
+    console.log('positivo');
+    
+ } else if (number < 0){
+
+    console.log('negativo');
+
+} else {
+    console.log('zero');
+
+}
