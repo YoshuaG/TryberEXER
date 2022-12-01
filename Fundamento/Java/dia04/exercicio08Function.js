5b339ebe72e5ec9fa4f1f02ec8fc4aee33b53573
YoshuaG <yoshua.gar@gmail.com>
2022-12-01 19:24:38 -0300

    resolve somadenumeros

--- Fundamento/Java/dia04/exercicio08Function.js
+++ Fundamento/Java/dia04/exercicio08Function.js
@@ -2,18 +2,16 @@
 // Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 // Valor esperado no retorno da função: 2.
 
-function repetidos(number){
+function repetidos(number) {
     let contagem1 = 0;
     let contagem2 = 0;
     let contagem3 = 0;
-    for(let index in number){
+    for (let index in number) {
         let verificar = number[index];
-        for(let index2 in number){
-            if(verificar === number[index2]){
-                contagem2 += 1;
-            }
+        for (let index2 in number) {
+            if (verificar === number[index2]) { contagem2 += 1; }
         }
-        if(verificar > contagem1){
+        if (verificar > contagem1) {
             contagem1 = contagem2;
         }
         contagem2 = 0;
